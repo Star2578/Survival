1aecf4c4651c3405570f3fc1d966429dfb5d6fc8
Star2578 <nuttykingger@gmail.com>
2022-07-15 17:19:23 +0700

    Generating Log and Rock

--- Assets/Script/MapManager.cs
+++ Assets/Script/MapManager.cs
@@ -8,10 +8,15 @@ public class MapManager : MonoBehaviour
     private static MapManager _instance;
     public static MapManager Instance { get { return _instance; } }
 
+    [Header("Spawn Overlay")]
     public OverlayTile overlaytilePf;
     public GameObject overlayContainer;
 
     public Dictionary<Vector2Int, OverlayTile> map = new Dictionary<Vector2Int, OverlayTile>();
+
+    [Header("Spawn Object")]
+    public GameObject objectContainer;
+    public List<GameObject> objectToSpawn = new List<GameObject>();
 
     private void Awake()
     {
@@ -50,6 +55,29 @@ public class MapManager : MonoBehaviour
                         OverlayTile.GetComponent<SpriteRenderer>().sortingOrder = tileMap.GetComponent<TilemapRenderer>().sortingOrder + 1;
                         map.Add(tileKey, OverlayTile);
                         OverlayTile.HideTile();
+                    }
+                }
+            }
+        }
+
+        for (int z = bounds.max.z; z >= bounds.min.z ; z--)
+        {
+            for (int x = bounds.min.x; x < bounds.max.x; x++)
+            {
+                for (int y = bounds.min.y; y < bounds.max.y; y++)
+                {
+                    var tileLocation = new Vector3Int(x, y, z);
+                    var tileKey = new Vector2Int(x, y);
+                    Vector3 place = tileMap.CellToWorld(tileLocation);
+
+                    var Randomizer = Random.Range(0, 99);
+                    if (tileMap.HasTile(tileLocation) && Randomizer > 80)
+                    {
+                        var obj = Instantiate(objectToSpawn[Random.Range(0, objectToSpawn.Count)], objectContainer.transform);
+                        var cellWorldPos = tileMap.GetCellCenterWorld(tileLocation);
+
+                        obj.transform.position = new Vector3(cellWorldPos.x, cellWorldPos.y, cellWorldPos.z + 1);
+                        obj.GetComponent<SpriteRenderer>().sortingOrder = tileMap.GetComponent<TilemapRenderer>().sortingOrder + 1;
                     }
                 }
             }
